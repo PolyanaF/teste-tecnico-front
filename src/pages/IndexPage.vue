6c92ea7fec93d42f9ae9b47995477979a7ab90bd
Polyana Feitosa <45500017+PolyanaF@users.noreply.github.com>
2024-10-15 20:58:08 -0300

    Iniciando o layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,43 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    
+    
+    <div class="container">
+      <div class="header">
+        <h3>Cadastro de unidade de medida</h3>
+        <q-btn push color="primary" label="Nova medida" />
+      </div>
+    </div>
+
+    <div class="divTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Código ERP</th>
+            <th>Sigla</th>
+            <th>Descricao</th>
+            <th>Status</th>
+            <th class="acao">Editar</th>
+            <th class="acao">Excluir</th>
+            <!-- <q-btn push color="red" icon="delete" class="acao"/> -->
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+    <div class="modal-container">
+      <div class="modal">
+        <q-form>
+          <q-input outlined v-model="text" label="cód ERP" />
+          <q-input outlined v-model="text" label="Sigla*" />
+          <q-input outlined v-model="text" label="Descricao*" />
+          <q-btn push color="primary" label="CADASTRAR" />
+        </q-form>
+      </div>
+    </div>
+
   </q-page>
 </template>
 
